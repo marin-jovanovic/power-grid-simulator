527ab1ea183e700910c100dcbdbd020c89c06948
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-12-13 14:21:46 +0100

    :recycle: refactor

--- util/address_manager.py
+++ util/address_manager.py
@@ -1,12 +1,6 @@
 """
 utils for address translations
 """
-import asyncio
-
-from hat.aio import run_asyncio
-from hat.drivers import iec104
-
-ADDRESSES = []
 
 
 class Address:
@@ -24,42 +18,6 @@ class Address:
 
     def formatted_name(self):
         return Address.get_as_csv(self.asdu_address, self.io_address)
-
-
-async def load_addresses(connection=None):
-    global ADDRESSES
-
-    if not connection:
-        address = iec104.Address('127.0.0.1', 19999)
-
-        connection = await iec104.connect(address)
-
-    raw_data = await connection.interrogate(asdu_address=65535)
-
-    for result in raw_data:
-        asdu_address = result.asdu_address
-        io_address = result.io_address
-
-        ADDRESSES.append(Address(asdu_address, io_address))
-
-    [print(i.formatted_name()) for i in ADDRESSES]
-
-
-async def connect():
-    address = iec104.Address('127.0.0.1', 19999)
-
-    while True:
-
-        try:
-            connection = await iec104.connect(address)
-            return connection
-
-        except ConnectionRefusedError:
-            n = 3
-            for i in range(n):
-                print("trying to reconnect in", n - i)
-                await asyncio.sleep(1)
-            print("reconnecting\n")
 
 
 async def async_main():
@@ -112,7 +70,7 @@ async def async_main():
 
 
 def main():
-    run_asyncio(async_main())
+    address = Address('127.0.0.1', 19999)
 
 
 if __name__ == '__main__':
