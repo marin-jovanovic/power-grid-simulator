a8806745f25b9ed6890e111aae24e844eea9082a
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-02-14 15:41:33 +0100

    :recycle: refactor

--- py/protocols/iec_104/iec_104_client.py
+++ py/protocols/iec_104/iec_104_client.py
@@ -187,3 +187,62 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# address = iec104.Address('127.0.0.1', 19999)
+# while True:
+#     try:
+#         connection = await iec104.connect(address)
+#         break
+#     except:
+#         continue
+#
+# try:
+#     raw_data = await connection.interrogate(asdu_address=65535)
+# except:
+#     address = iec104.Address('127.0.0.1', 19999)
+#     connection = await iec104.connect(address)
+#     raw_data = await connection.interrogate(asdu_address=65535)
+#
+# [print(i) for i in raw_data]
+#
+# return
+
+# async def async_main():
+#
+#     await asyncio.sleep(5)
+#     print("start")
+#     address = iec104.Address('127.0.0.1', 19999)
+#     connection = await iec104.connect(address)
+#     raw_data = await connection.interrogate(asdu_address=65535)
+#     [print(i) for i in raw_data]
+#     return
+#
+#
+#
+#     connection = await connect()
+#     breakpoint()
+#
+#     raw_data = await connection.interrogate(asdu_address=65535)
+#     print(len(raw_data))
+#     print("r", raw_data)
+#
+#     while True:
+#
+#         try:
+#             print("fetch")
+#             raw_data = await connection.receive()
+#             print("r", raw_data)
+#
+#         except ConnectionError:
+#             print("connection lost")
+#
+#             connection = await connect()
+#
+#
+# def main():
+#     address = Address('127.0.0.1', 19999)
+#
+#
+# if __name__ == '__main__':
+#     main()
